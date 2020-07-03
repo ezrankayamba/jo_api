4f4515cb4f37b86fa8ceba6d6cfc999d3e3d8e8f
ezrankayamba <ezrankayamba@gmail.com>
2020-07-03 18:50:23 +0300

    Fix dupplicates and additional params

--- backend_rest/jo/views.py
+++ backend_rest/jo/views.py
@@ -8,6 +8,7 @@ import datetime
 import requests
 import json
 import configparser
+from datetime import datetime, timedelta
 
 # Create your views here.
 
@@ -55,12 +56,10 @@ class JoAPI(APIView):
         new_dt_str = dt_obj.strftime('%Y-%m-%d')
         return new_dt_str
 
-    def get_data(self, ip_address=None, start_date=None, start_jo=None):
-        sql = 'SELECT u.fullname, u.joDivision, a.joStatus, a.joAuthoriserEmail, a.joApprovedDate, e.ne_commonname, e.ne_hostname, e.ne_ip, j.* FROM `jobs` j left join users u on j.requestor = u.username left join joauthoriser a on j.jo_id = a.jo_id left join jo_affected_ne f on j.jo_id=f.af_jo_id left join jo_network_elements e on j.jo_id = f.af_jo_id AND f.af_ne_id = e.ne_id where 1=1 '
+    def get_data(self, ip_address=None, start_date=None, start_jo=None, range_hours=None):
+        print(f'IP: {ip_address}, Date: {start_date}, StartJO: {start_jo}, RangeHours: {range_hours}')
+        sql = "SELECT u.fullname, u.joDivision, j.* FROM `jobs` j left join users u on j.requestor = u.username where j.jobstatus in ('BROADCASTED','SUCCESSFUL','UNSUCCESSFUL','ONGOING','PAUSED')"
         params = []
-        if ip_address:
-            sql = f'{sql} and ne_ip = %s '
-            params.append(ip_address)
 
         if start_date:
             sql = f'{sql} and thedate >= %s '
@@ -70,23 +69,41 @@ class JoAPI(APIView):
             sql = f'{sql} and j.jo_id >= %s '
             params.append(start_jo)
 
+        if range_hours:
+            d = datetime.today() - timedelta(hours=range_hours)
+            new_start_date = d.strftime('%Y-%m-%d %H:%M:%S')
+            sql = f'{sql} and thedate >= %s '
+            params.append(f'{new_start_date}')
+
         rs = models.Jobs.objects.raw(sql, params)
         data = []
         for row in rs:
             approval = []
-            for appr in models.Joauthoriser.objects.filter(jo_id=row.jo_id):
+            disapproved = False
+            for appr in models.Joauthoriser.objects.filter(jo_id=row.jo_id).order_by('joapproveddate'):
                 approval.append({
                     'ApprovalStatus': appr.jostatus,
                     'Approver': appr.joauthoriseremail,
                     'ApprovalDate': self.str_date_to_fmt(appr.joapproveddate),
                 })
+                if appr.jostatus == 'DISAPPROVED':
+                    disapproved = True
+            if row.jobstatus == 'PAUSED' and not disapproved:
+                continue
+
             sys_changed = []
             sys_sql = f'SELECT f.af_ne_id, e.* from jo_affected_ne f left join jo_network_elements e on f.af_ne_id = e.ne_id where f.af_jo_id = %s'
+            ip_found = False
             for ne in models.JoAffectedNe.objects.raw(sys_sql, [row.jo_id]):
                 sys_changed.append({
                     'SystemName': ne.ne_commonname,
                     'IpAddress': ne.ne_ip,
                 })
+                if ne.ne_ip == ip_address:
+                    ip_found = True
+            if ip_address and not ip_found:
+                continue
+
             data.append({
                 'Identifier': row.jo_id,
                 'Requestor': row.fullname,
@@ -96,21 +113,26 @@ class JoAPI(APIView):
                 'CABRefNumber': row.support_ref_number,
                 'JONumber': row.jo_number,
                 'JOType': row.job_status,
+                'JOStatus': row.jobstatus,
                 'JOBTitle': row.subject,
                 'Approval': approval,
-                'SystemsToBeChanged': sys_changed,
+                'JOCategory': row.change_type,
+                'Purpose': row.purpose,
+                'SystemsImpacted': row.purpose,
+                'SystemsToBeChanged': row.affected_network_element,
                 'MitigationPlan': row.mitigation_in_place,
             })
         return data
 
     def post(self, request):
-        print("API User: ", request.user)
+        print("API User: ", request.user, ", Request: ", request.data)
         params = request.data['Params'] if 'Params' in request.data else None
         if params:
             ip_address = params['IPAddress'] if 'IPAddress' in params else None
             start_date = params['StartDate'] if 'StartDate' in params else None
             start_jo = params['StartJO'] if 'StartJO' in params else None
-        data = self.get_data(ip_address, start_date, start_jo)
+            range_hours = params['RangeHours'] if 'RangeHours' in params else None
+        data = self.get_data(ip_address, start_date, start_jo, range_hours)
         return Response({
             'statusCode': 200,
             'statusMessage': f'Success',
